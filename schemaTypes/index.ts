0bd8d3c9a314565b7232b9a235dff28f6d503e9d
Brandon Hernández <isahecam@gmail.com>
2026-08-18 21:19:57 -0600

    feat: define schema types for managing various document types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import blockContent from "./objects/block-content";
+import { bioType } from "./bio";
+import { experienceType } from "./experienceType";
+import { projectType } from "./projectType";
+import { technologyType } from "./technologyType";
+
+export const schemaTypes = [bioType, experienceType, projectType, technologyType, blockContent];
